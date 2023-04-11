cd7667f731785a6af7b17dfcd1b396c1a83cd44d
chinmayanandNimap <chinmayanand@nimapinfotech@gmail.com>
2023-04-11 23:55:49 +0530

    user CRUD operations

--- src/handler/User/userAuth.js
+++ src/handler/User/userAuth.js
@@ -1,13 +1,97 @@
 const userModel = require("../../models/user.module");
 
-exports.getUserDetails = async (req, res) => {
+exports.getAllUserDetails = async (req, res) => {
   try {
-    res.send("user data Gurunath");
+    const allUserData = await userModel.find();
+    res.send(allUserData);
   } catch (err) {
-    res.send(err);
+    res.send({ err });
   }
 };
 
 exports.createUser = async (req, res) => {
-    
-}
+  try {
+    const {
+      firstname,
+      lastname,
+      gender,
+      phoneNumber,
+      password,
+      userType,
+      role,
+    } = req.body;
+    const obj = {
+      firstname: firstname,
+      lastname: lastname,
+      gender: gender,
+      phoneNumber: phoneNumber,
+      password: password,
+      userType: userType,
+      role: role,
+    };
+    await userModel.create(obj);
+
+    res.send({
+      message: "created Successfully",
+      status: true,
+      status_code: 200,
+    });
+  } catch (err) {
+    res.send({ err });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const userData = await userModel.findById(id);
+
+    res.send(userData);
+  } catch (err) {
+    res.send({ err });
+  }
+};
+
+exports.deleteUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await userModel.deleteOne({ _id: id });
+    return { message: "success", status: true };
+  } catch (err) {
+    return { err };
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const {
+      id,
+      firstname,
+      lastname,
+      gender,
+      phoneNumber,
+      password,
+      userType,
+      role,
+    } = req.body;
+
+    const isUserPresent = await userModel.findById(id);
+    console.log("isUserPresent", isUserPresent);
+    if (!isUserPresent) {
+      return { message: "user not found" };
+    }
+    if (firstname) isUserPresent.firstname = firstname;
+    if (lastname) isUserPresent.lastname = lastname;
+    if (gender) isUserPresent.gender = gender;
+    if (phoneNumber) isUserPresent.phoneNumber = phoneNumber;
+    if (userType) isUserPresent.userType = userType;
+    if (role) isUserPresent.role = role;
+
+    await isUserPresent.save();
+    return { message: "success", status: true };
+  } catch (err) {
+    console.log("CATCH", err);
+    return { message: "failed", status: false };
+  }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,16 +1,42 @@
-const { getUserDetails } = require("../handler/User/userAuth");
+const {
+  getAllUserDetails,
+  createUser,
+  getUserById,
+  deleteUserById,
+  updateUser,
+} = require("../handler/User/userAuth");
 const { routesRegister } = require("../utils/utils");
 
 module.exports = async (fastify) => {
-    fastify.log.info('User route registration started');
+  fastify.log.info("User route registration started");
 
-    await routesRegister(fastify, [
-        {
-            method: 'GET',
-            path: '/user', // Example for creating API routes
-            handler: getUserDetails,
-        }
-    ]);
+  await routesRegister(fastify, [
+    {
+      method: "GET",
+      path: "/user",
+      handler: getAllUserDetails,
+    },
+    {
+      method: "POST",
+      path: "/user",
+      handler: createUser,
+    },
+    {
+      method: "GET",
+      path: "/user/:id",
+      handler: getUserById,
+    },
+    {
+      method: "DELETE",
+      path: "/user/:id",
+      handler: deleteUserById,
+    },
+    {
+      method: "PUT",
+      path: "/user",
+      handler: updateUser,
+    },
+  ]);
 
-    fastify.log.info('User route registration completed');
+  fastify.log.info("User route registration completed");
 };
